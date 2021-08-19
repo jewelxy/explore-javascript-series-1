99e93929ea09aa34161fe0c41353a1a638be0cc1
jewelranaxy <dhs.shahin@gmail.com>
2021-08-19 11:36:09 +0600

    Event listener function

--- Event Listener/JSFAQ/function.js
+++ Event Listener/JSFAQ/function.js
@@ -1,8 +1,13 @@
-console.log(12);
-nums = [5,8,2,12,17];
-
-for (let i=0; i<nums.length; i++) {
-    const num = nums[i];
-    console.log(num)
-    
+function evenify(arr_names){
+    for (let i=0; i<arr_names.length; i++) {
+        const value = arr_names[i];
+        if(value % 2== 0){
+            console.log(value,"Even Number");
+        }else{
+            console.log(value, "Odd Number");
+        } 
+    }          
 }
+nums = [5,8,2,12,9,13,15,7];
+evenify(nums);
+
